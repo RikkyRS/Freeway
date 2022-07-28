42848ac649b327b34d998fbea3ee59c3cb91b9df
Henrique Rodrigues <102548626+RikkyRS@users.noreply.github.com>
2022-07-28 09:35:12 -0300

    Create Imagens.js

---
+++ Imagens.js
@@ -0,0 +1,23 @@
+//setup
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3
+
+//Sons
+let trilhasonora;
+let colidiu;
+let pontos;
+
+function preload(){
+  imagemDaEstrada = loadImage("Imagens/estrada.png");
+  imagemDoAtor = loadImage("Imagens/ator-1.png");
+  imagemCarro = loadImage("Imagens/carro-1.png");
+  imagemCarro2 = loadImage("Imagens/carro-2.png");
+  imagemCarro3 = loadImage("Imagens/carro-3.png");
+  imagemCarros =[imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+  trilhasonora = loadSound("Sons/trilha.mp3");
+  colidiu = loadSound("Sons/colidiu.mp3");
+  ponto = loadSound("Sons/pontos.wav");
+}
